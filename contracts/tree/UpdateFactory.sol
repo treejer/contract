47d157fb0170d88262cf0a3899dfbbeecf30936f
Amir Habibzadeh <habibzade.amir@gmail.com>
2020-06-29 18:27:24 +0430

    update added

---
+++ contracts/tree/UpdateFactory.sol
@@ -0,0 +1,35 @@
+pragma solidity >=0.4.21 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "./TreeFactory.sol";
+
+contract UpdateFactory is TreeFactory {
+
+    event UpdateAdded(uint256 updateId, uint256 treeId, string imageHash);
+    event UpdateAccepted(uint256 updateId);
+
+    struct Update {
+        uint treeId;
+        string imageHash;
+        uint updateDate;
+        uint8 status;
+    }
+
+    Update[] public updates;
+    mapping(uint256 => uint256[]) public treeUpdates;
+
+    uint public lastFundedTreeIndex;  
+
+    function post(uint256 _treeId, string calldata _imageHash) external {
+       uint256 id = updates.push(Update(_treeId, _imageHash, now, 0)) - 1;
+       treeUpdates[_treeId].push(id);
+
+       emit UpdateAdded(id, _treeId, _imageHash);
+    }
+    
+    function acceptUpdate(uint256 _updateId) external {
+        updates[_updateId].status = 1;
+        emit UpdateAccepted(_updateId);
+    }
+    
+}
---
+++ test/updatefactory.js
@@ -0,0 +1,85 @@
+const UpdateFactory = artifacts.require("UpdateFactory");
+const assert = require("chai").assert;
+const truffleAssert = require('truffle-assertions');
+const Units = require('ethereumjs-units');
+
+
+contract('UpdateFactory', (accounts) => {
+    let updateInstance;
+
+    const deployerAccount = accounts[0];
+    const ownerAccount = accounts[1];
+
+    beforeEach(async () => {
+        updateInstance = await UpdateFactory.new({ from: deployerAccount });
+
+    });
+
+    afterEach(async () => {
+        // await treeInstance.kill({ from: ownerAccount });
+    });
+
+    function addTree(name = null) {
+        let typeId = 0;
+        let gbId = 0;
+        name = name !== null ? name : 'firstTree';
+        let latitude = '38.0962';
+        let longitude = '46.2738';
+        let plantedDate = '2020/02/20';
+        let birthDate = '2020/02/20';
+        let height = '1';
+        let diameter = '1';
+
+        return updateInstance.add(
+            typeId,
+            gbId,
+            [
+                name,
+                latitude,
+                longitude,
+                plantedDate,
+                birthDate
+            ],
+            [
+                height,
+                diameter,
+            ],
+            { from: ownerAccount });
+    }
+
+    it("should add updates", async () => {
+
+        addTree('firstTree');
+        let treeId = 0;
+        let imageHash = '0x14dsahjdauhdiw012564';
+
+        let tx = await updateInstance.post(treeId, imageHash, { from: ownerAccount });
+
+        truffleAssert.eventEmitted(tx, 'UpdateAdded', (ev) => {
+            return ev.updateId.toString() === '0' && ev.treeId.toString() === treeId.toString() && ev.imageHash.toString() === imageHash.toString();
+        });
+
+    });
+
+
+    it("should accept update", async () => {
+
+        addTree('firstTree');
+        let treeId = 0;
+        let imageHash = '0x14dsahjdauhdiw012564';
+
+        let tx = await updateInstance.post(treeId, imageHash, { from: ownerAccount });
+
+
+        let tx1 = await updateInstance.acceptUpdate(0, { from: ownerAccount });
+
+
+        truffleAssert.eventEmitted(tx1, 'UpdateAccepted', (ev) => {
+            return ev.updateId.toString() === '0';
+        });
+
+    });
+
+
+
+});

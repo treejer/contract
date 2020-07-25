4226ce57e313ff3df3cd1c1ce86fba05f83f47d6
Amir Habibzadeh <habibzade.amir@gmail.com>
2020-07-25 12:08:48 +0430

    deploy contract added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,30 @@
-var TreeType = artifacts.require("./TreeType.sol");
-var GBFactory = artifacts.require("./GBFactory.sol");
-var TreeFactory = artifacts.require("./TreeFactory.sol");
+var AccessRestriction = artifacts.require("AccessRestriction.sol");
+var GBFactory = artifacts.require("GBFactory.sol");
+var TreeType = artifacts.require("TreeType.sol");
+var TreeFactory = artifacts.require("TreeFactory.sol");
+var UpdateFactory = artifacts.require("UpdateFactory.sol");
+var O2Factory = artifacts.require("O2Factory.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(TreeType);
-  deployer.deploy(GBFactory);
-  deployer.deploy(TreeFactory);
+module.exports = async function(deployer) {
+
+  let treeTypeAddress;
+  let treeAddress;
+  let updateAddress;
+
+  await deployer.deploy(AccessRestriction);
+
+  await deployer.deploy(GBFactory);
+
+  await deployer.deploy(TreeType)
+    .then(() => { treeTypeAddress = TreeType.address; });
+
+  await deployer.deploy(TreeFactory)
+    .then(() => { treeAddress = TreeFactory.address; });  
+
+  await deployer.deploy(UpdateFactory)
+    .then(() => { updateAddress = UpdateFactory.address; }); 
+
+  await deployer.deploy(O2Factory, treeTypeAddress, treeAddress, updateAddress);
+
+
 };

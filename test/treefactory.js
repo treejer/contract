a3ae170af89e917a8905af71009b25ef8d5df686
Amir Habibzadeh <habibzade.amir@gmail.com>
2020-12-16 15:42:49 +0330

    add check size of code to tests

--- test/treefactory.js
+++ test/treefactory.js
@@ -44,6 +44,18 @@ contract('TreeFactory', (accounts) => {
         // await treeInstance.kill({ from: ownerAccount });
     });
 
+    it("get the size of the contract", function () {
+        return TreeFactory.deployed().then(function (instance) {
+            var bytecode = instance.constructor._json.bytecode;
+            var deployed = instance.constructor._json.deployedBytecode;
+            var sizeOfB = bytecode.length / 2;
+            var sizeOfD = deployed.length / 2;
+            console.log("size of bytecode in bytes = ", sizeOfB);
+            console.log("size of deployed in bytes = ", sizeOfD);
+            console.log("initialisation and constructor code in bytes = ", sizeOfB - sizeOfD);
+        });
+    });
+
     it("should add tree", async () => {
 
         await Common.addPlanter(arInstance, ownerAccount, deployerAccount);

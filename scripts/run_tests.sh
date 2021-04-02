2dbee025c05f3203c650f538ca62ab4be0f3e46d
Amir Habibzadeh <habibzade.amir@gmail.com>
2021-04-02 13:28:08 +0430

    i think it works

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -11,11 +11,6 @@ cleanup() {
   if [ -n "$ganache_pid" ] && ps -p $ganache_pid > /dev/null; then
     kill -9 $ganache_pid
   fi
-
-  # Kill the ganache instance that we started (if we started one and if it's still running).
-  if [ -n "$gsn_pid" ] && ps -p $gsn_pid > /dev/null; then
-    kill -9 $gsn_pid
-  fi
 }
 
 ganache_port=8545
@@ -43,14 +38,6 @@ start_ganache() {
 
 start_gsn() {
   node_modules/.bin/gsn start
-  gsn_pid=$!
-
-  echo "Waiting for gsn to launch ..."
-
-  while ! gsn_running; do
-    sleep 0.9 # wait for 1/10 of the second before check again
-  done
-
   echo "GSN started!"
 }
 
@@ -60,13 +47,14 @@ else
   echo "Starting our own ganache instance"
   start_ganache
   sleep 5
-  if gsn_running; then
-    echo "Using existing gsn instance"
-  else
-    echo "Starting our own gsn instance"
-    start_gsn
-    sleep 5
-  fi
+fi
+
+ if gsn_running; then
+  echo "Using existing gsn instance"
+else
+  echo "Starting our own gsn instance"
+  start_gsn
+  sleep 10
 fi
 
 

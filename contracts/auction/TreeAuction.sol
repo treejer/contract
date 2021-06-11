af9d0ef39964fb151a952c325789cf764311c59d
ali <abdolazim35@yahoo.com>
2021-06-12 00:07:11 +0430

    auction

--- contracts/auction/TreeAuction.sol
+++ contracts/auction/TreeAuction.sol
@@ -5,19 +5,17 @@ pragma solidity ^0.6.9;
 contract TreeAuction {
     address payable treasuryAddress;
     struct Auction {
-      uint256 auctionId; 
-      uint256 treeId; 
-      address bider; 
-      
-      bytes32 status;
-      uint64 startDate; 
-      uint64 endDate; 
-      uint64 highestBid; 
-      uint32 initialPrice;
-      uint32 bidInterval; 
-}
+        uint256 auctionId;
+        uint256 treeId;
+        address bider;
+        bytes32 status;
+        uint64 startDate;
+        uint64 endDate;
+        uint64 highestBid;
+        uint32 initialPrice;
+        uint32 bidInterval;
+    }
 
-mapping(address=>Auction) auctios;
-mapping(address=>uint64) pendingWithdraw;
-
+    mapping(address => Auction) auctios;
+    mapping(address => uint64) pendingWithdraw;
 }

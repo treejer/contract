e0444cac50cd137ac97a5243110349464fbfb4b8
smuggler <mahdigorbanzadeh.1378@yahoo.com>
2021-06-11 16:56:32 -0400

    add withdraw and manualWithdraw auctionEnd in auction

--- contracts/auction/TreeAuction.sol
+++ contracts/auction/TreeAuction.sol
@@ -38,4 +38,54 @@ function initialize(address _accessRestrictionAddress) public initializer {
   accessRestriction = candidateContract;
 }
 
+function _withdraw(uint64 auctionId, uint64 oldbid, address payable oldbidder) private {
+    uint32 size; 
+    assembly { 
+    size := extcodesize(_addr)
+    }
+    if(size>0)
+    {
+      pendingWithdraw[oldbidder] += oldbid;
+    }else if (!oldbidder.transfer(amount)) {
+      pendingWithdraw[oldbidder] += oldbid;    
+    }
+
+} 
+
+function manualWithdraw() external returns (bool) {
+  uint64 amount = pendingWithdraw[msg.sender];
+
+  if (amount > 0) {
+      pendingWithdraw[msg.sender] = 0;
+
+      if (!msg.sender.transfer(amount)) {
+          pendingWithdraw[msg.sender] = amount;
+          return false;
+      }
+  }
+  return true;
+}
+
+function auctionEnd(uint64 auctionId) external {
+
+  accessRestriction.ifAdmin(msg.sender);
+
+
+  Auction memory localAuction = auctios[auctionId];
+
+  require(now >= localAuction.endDate, "Auction not yet ended.");
+  require(keccak256(abi.encodePacked((localAuction.status))) == keccak256(abi.encodePacked((bytes32("end")))), "auctionEnd has already been called.");
+  require(localAuction.bider != address(0),"No refer to auction");
+
+  genesisTree.updateOwner(localAuction.treeId,localAuction.bider);
+  genesisTreeFund.update(localAuction.treeId,localAuction.highestBid);
+
+  auctios[auctionId]= bytes32("end");
+
+  treasuryAddress.transfer(localAuction.highestBid);
+
+}
+
+
+
 }

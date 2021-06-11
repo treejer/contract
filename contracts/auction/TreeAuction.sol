f4115664884f645464f2054c731be1c310819684
smuggler <mahdigorbanzadeh.1378@yahoo.com>
2021-06-11 15:36:12 -0400

    add constructor in auction

--- contracts/auction/TreeAuction.sol
+++ contracts/auction/TreeAuction.sol
@@ -2,13 +2,18 @@
 
 pragma solidity ^0.6.9;
 
-contract TreeAuction {
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+
+import "../access/IAccessRestriction.sol";
+
+contract TreeAuction is Initializable {
+
+
     address payable treasuryAddress;
     struct Auction {
       uint256 auctionId; 
       uint256 treeId; 
       address bider; 
-      
       bytes32 status;
       uint64 startDate; 
       uint64 endDate; 
@@ -17,7 +22,20 @@ contract TreeAuction {
       uint32 bidInterval; 
 }
 
+//
 mapping(address=>Auction) auctios;
 mapping(address=>uint64) pendingWithdraw;
+
+IAccessRestriction public accessRestriction;
+
+// IGenesisTree public genesisTree;
+
+
+function initialize(address _accessRestrictionAddress) public initializer {
+  IAccessRestriction candidateContract =
+  IAccessRestriction(_accessRestrictionAddress);
+  require(candidateContract.isAccessRestriction());
+  accessRestriction = candidateContract;
+}
 
 }

a078e73ed02727ec602d99ac4a5e520843e0c5bf
ali <abdolazim35@yahoo.com>
2021-06-17 20:06:38 +0430

    genesis tree base

---
+++ contracts/genesisTree/GenesisTree.sol
@@ -0,0 +1,59 @@
+// // SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.9;
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+import "../access/IAccessRestriction.sol";
+
+contract GenesisTree is Initializable {
+    bool public isGenesisTree;
+    IAccessRestriction public accessRestriction;
+    struct GenTree {
+        address planterId;
+        uint256 gbId;
+        uint256 treeType;
+        uint8 gbType;
+        uint8 ownershipStatus;
+        uint16 provideStatus;
+        uint16 treeStatus;
+        uint16 countryCode;
+        uint64 plantDate;
+        uint64 birthDate;
+        uint64 lastUpdate;
+        string treeSpecs;
+    }
+    struct UpdateGenTree {
+        string updateSpecs;
+        uint64 updateDate;
+        uint64 updateStatus;
+    }
+    mapping(uint256 => GenTree) genTrees;
+    mapping(uint256 => UpdateGenTree) updateGenTrees;
+
+    function initialize(address _accessRestrictionAddress) public initializer {
+        IAccessRestriction candidateContract =
+            IAccessRestriction(_accessRestrictionAddress);
+        require(candidateContract.isAccessRestriction());
+        isGenesisTree = true;
+        accessRestriction = candidateContract;
+    }
+
+    function addTree() external {
+        accessRestriction.ifAdmin(msg.sender);
+    }
+
+    function asignTreeToPlanter() external {
+        accessRestriction.ifAdmin(msg.sender);
+    }
+
+    function plantTree() external {}
+
+    function verifyPlant() external {}
+
+    function updateTree() external {}
+
+    function verifyUpdate() external {}
+
+    function checkAndSetProvideStatus() external {}
+
+    function updateOwner() external {}
+}
---
+++ contracts/genesisTree/IGenesisTree.sol
@@ -0,0 +1,7 @@
+// // SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.9;
+
+interface IGenesisTree {
+    function isGenesisTree() external view returns (bool);
+}

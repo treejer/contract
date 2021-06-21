40450159666921d630c6b0e56e41656a9255d05c
ali <abdolazim35@yahoo.com>
2021-06-21 16:58:32 +0430

    interface done

--- contracts/genesisTree/IGenesisTree.sol
+++ contracts/genesisTree/IGenesisTree.sol
@@ -3,6 +3,11 @@
 pragma solidity ^0.6.9;
 
 interface IGenesisTree {
+    event PlantTree(uint256 treeId, address planter);
+    event VerifyPlant(uint256 treeId, uint256 updateStatus);
+    event UpdateTree(uint256 treeId);
+    event VerifyUpdate(uint256 treeId, uint64 updateStatus);
+
     function isGenesisTree() external view returns (bool);
 
     function setGBFactoryAddress(address _address) external;

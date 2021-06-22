f04fdba42beb012397602923fa7b0cea8a0710f7
smuggler <mahdigorbanzadeh.1378@yahoo.com>
2021-06-22 14:37:06 +0430

    add addFundDistributionModel in treasuryManager

--- contracts/treasuryManager/TreasuryManager.sol
+++ contracts/treasuryManager/TreasuryManager.sol
@@ -2,10 +2,19 @@
 
 pragma solidity ^0.6.9;
 
+import "@openzeppelin/contracts-upgradeable/utils/SafeCastUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "../access/IAccessRestriction.sol";
 
 contract TreasuryManager is Initializable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    using SafeCastUpgradeable for uint256;
+    using SafeMathUpgradeable for uint64;
+    using SafeMathUpgradeable for uint16;
+
+    CountersUpgradeable.Counter private fundDistributionCount;
     bool public isTreasuryManager;
     AssignModel[] public assignModels;
     uint256 public maxAssignedIndex;
@@ -61,7 +70,49 @@ contract TreasuryManager is Initializable {
         accessRestriction = candidateContract;
     }
 
-    function addFundDistributionModel() external onlyAdmin {}
+    function addFundDistributionModel(
+        uint16 _planter,
+        uint16 _referral,
+        uint16 _treeResearch,
+        uint16 _localDevelop,
+        uint16 _rescueFund,
+        uint16 _treejerDevelop,
+        uint16 _otherFund1,
+        uint16 _otherFund2
+    ) external onlyAdmin {
+        require(
+            _add(
+                _add(
+                    _add(
+                        _add(
+                            _add(
+                                _add(_add(_planter, _referral), _treeResearch),
+                                _localDevelop
+                            ),
+                            _rescueFund
+                        ),
+                        _treejerDevelop
+                    ),
+                    _otherFund1
+                ),
+                _otherFund2
+            ) == 10000,
+            "sum must be 10000"
+        );
+
+        fundDistributions[fundDistributionCount.current()] = FundDistribution(
+            _planter,
+            _referral,
+            _treeResearch,
+            _localDevelop,
+            _rescueFund,
+            _treejerDevelop,
+            _otherFund1,
+            _otherFund2
+        );
+
+        fundDistributionCount.increment();
+    }
 
     function assignTreeFundDistributionModel() external onlyAdmin {}
 
@@ -72,4 +123,11 @@ contract TreasuryManager is Initializable {
     function fundPlanter() external {}
 
     function withdrawTreejerDevelop() external {}
+
+    function _add(uint16 a, uint16 b) private pure returns (uint16) {
+        uint16 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
 }

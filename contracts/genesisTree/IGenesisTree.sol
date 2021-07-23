8658680b98f709f0e96de8a8696496bd3848a83c
smuggler <mahdigorbanzadeh.1378@yahoo.com>
2021-07-23 16:36:09 +0430

    add comment in IGenesisTree

--- contracts/genesisTree/IGenesisTree.sol
+++ contracts/genesisTree/IGenesisTree.sol
@@ -44,13 +44,53 @@ interface IGenesisTree {
     //     address _owner
     // ) external;
 
+    /**
+     * @dev This function is called by planter who have planted a new tree
+     * The planter enters the information of the new tree
+     * Information is stored in The {regularTrees} mapping
+     * And finally the tree is waiting for approval
+     *
+     *
+     * @param _treeSpecs //TODO: what is _treeSpecs ??
+     * @param _birthDate birthDate of the tree
+     * @param _countryCode Code of the country where the tree was planted
+     */
+    function regularPlantTree(
+        string memory _treeSpecs,
+        uint64 _birthDate,
+        uint16 _countryCode
+    ) external;
+
+    /**
+     * @dev In this function, the admin approves or rejects the pending trees
+     * After calling this function, if the tree is approved the tree information will be transferred to the {genTrees}
+     *
+     * @param _regularTreeId
+     * @param isVerified Tree approved or not
+     */
     function verifyRegularPlant(uint256 _regularTreeId, bool isVerified)
         external;
 
+    /**
+     * @dev Transfer ownership of trees purchased by funders and Update the last tree sold
+     * This function is called only by the regularSell contract
+     *
+     * @param lastSold The last tree sold in the regular
+     * @param _owner Owner of a new tree sold in Regular
+     *
+     *
+     * @return The last tree sold after update
+     */
     function mintRegularTrees(uint256 lastSold, address _owner)
         external
         returns (uint256);
 
+    /**
+     * @dev Request to buy a tree with a specific Id already planted and this function transfer ownership to funder
+     * This function is called only by the regularSell contract
+     * @param _treeId Tree with special Id (The Id must be larger than the last tree sold)
+     * @param _owner Owner of a new tree sold in Regular
+     */
     function requestRegularTree(uint256 _treeId, address _owner) external;
 
     event TreePlanted(uint256 treeId, address planter);

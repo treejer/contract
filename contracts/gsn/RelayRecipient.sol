13bbdf5429ba099638fb29d659df113cfe34f167
smuggler <mahdigorbanzadeh.1378@yahoo.com>
2021-07-27 17:27:45 +0430

    change versionRecipient from view to pure function

--- contracts/gsn/RelayRecipient.sol
+++ contracts/gsn/RelayRecipient.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.7.6;
 import "@opengsn/contracts/src/BaseRelayRecipient.sol";
 
 contract RelayRecipient is BaseRelayRecipient {
-    function versionRecipient() external view override returns (string memory) {
+    function versionRecipient() external pure override returns (string memory) {
         return "2.2.0+treejer.irelayrecipient";
     }
 }

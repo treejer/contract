a9bf37c40c473450d5f0d5f0190e48af9c77b064
smuggler <mahdigorbanzadeh.1378@yahoo.com>
2021-07-29 17:12:32 +0430

    add Erc20

--- contracts/treasury/Funds.sol
+++ contracts/treasury/Funds.sol
@@ -10,7 +10,34 @@ import "../access/IAccessRestriction.sol";
 
 contract Funds is Initializable {
     bool public isFunds;
+    bytes32 public tokenName;
+
     IAccessRestriction public accessRestriction;
+
+    TotalFunds public totalFunds;
+
+    address payable public treeResearchAddress;
+    address payable public localDevelopAddress;
+    address payable public rescueFundAddress;
+    address payable public treejerDevelopAddress;
+    address payable public reserveFundAddress1;
+    address payable public reserveFundAddress2;
+
+    struct TotalFunds {
+        uint256 planterFund;
+        uint256 referralFund;
+        uint256 treeResearch;
+        uint256 localDevelop;
+        uint256 rescueFund;
+        uint256 treejerDevelop;
+        uint256 reserveFund1;
+        uint256 reserveFund2;
+    }
+
+    modifier onlyAdmin() {
+        accessRestriction.ifAdmin(msg.sender);
+        _;
+    }
 
     function initialize(address _accessRestrictionAddress) public initializer {
         IAccessRestriction candidateContract = IAccessRestriction(
@@ -21,5 +48,9 @@ contract Funds is Initializable {
 
         isFunds = true;
         accessRestriction = candidateContract;
+    }
+
+    function setToken(bytes32 _tokenName) external onlyAdmin {
+        tokenName = _tokenName;
     }
 }

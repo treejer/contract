ecd065e29f120ac8eeb307061f717bc7d57b432a
ali <abdolazim35@yahoo.com>
2021-08-10 22:28:25 +0430

    comminity gifts

--- contracts/communityGifts/ICommunityGifts.sol
+++ contracts/communityGifts/ICommunityGifts.sol
@@ -7,11 +7,32 @@ pragma solidity ^0.8.6;
 interface ICommunityGifts {
     function isCommunityGifts() external view returns (bool);
 
+    function planterFund() external view returns (uint256);
+
+    function referralFund() external view returns (uint256);
+
+    function communityGifts(address _address)
+        external
+        view
+        returns (
+            uint32 symbol,
+            bool claimed,
+            bool exist
+        );
+
+    function claimedCount() external view returns (uint256);
+
+    function expireDate() external view returns (uint256);
+
+    function giftCount() external view returns (uint256);
+
     function setTreeAttributesAddress(address _address) external;
 
     function setTreeFactoryAddress(address _address) external;
 
-    function setTreasuryAddress(address _address) external;
+    function setPlanterFundAddress(address _address) external;
+
+    function setGiftsRange(uint256 _startTreeId, uint256 _endTreeId) external;
 
     function updateGiftees(address _giftee, uint32 _symbol) external;
 
@@ -20,6 +41,8 @@ interface ICommunityGifts {
     function setExpireDate(uint256 _expireDate) external;
 
     function transferTree(address _giftee, uint32 _symbol) external;
+
+    function setPrice(uint256 _planterFund, uint256 _referralFund) external;
 
     event GifteeUpdated(address giftee);
     event TreeClaimed(uint256 treeId);

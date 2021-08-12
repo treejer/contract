9c5f16b2d297bfb3cc8b4ab2235abf39a89ed42f
ali <abdolazim35@yahoo.com>
2021-08-13 00:39:53 +0430

    treeFunded

--- migrations/2_deploy_accessRestriction.js
+++ migrations/2_deploy_accessRestriction.js
@@ -5,7 +5,6 @@ const AccessRestriction = artifacts.require("AccessRestriction.sol");
 
 module.exports = async function (deployer, network, accounts) {
   console.log("Deploying AccessRestriction...");
-  console.log("accounts[0]", accounts[0]);
 
   await deployProxy(AccessRestriction, [accounts[0]], {
     deployer,

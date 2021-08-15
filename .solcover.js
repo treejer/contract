e649a48afd7072ae2d62242c17fcdcce350337d9
smuggler <mahdigorbanzadeh.1378@yahoo.com>
2021-08-15 13:03:27 +0430

    coverage test config

--- .solcover.js
+++ .solcover.js
@@ -8,6 +8,7 @@ module.exports = {
   measureFunctionCoverage: true,
   istanbulReporter: ["html"],
   mocha: {
+    timeout: 0,
     grep: "@skip-on-coverage", // Find everything with this tag
     invert: true, // Run the grep's inverse set.
   },

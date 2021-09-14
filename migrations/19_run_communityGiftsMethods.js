b3e8ee8ea931ae36cb3f7d3ce04cc528f36ecf49
ali <abdolazim35@yahoo.com>
2021-09-14 13:23:25 +0430

    add migration

---
+++ migrations/19_run_communityGiftsMethods.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const CommunityGifts = artifacts.require("CommunityGifts.sol");
+const TreeAttribute = artifacts.require("TreeAttribute.sol");
+const TreeFactory = artifacts.require("TreeFactory.sol");
+const PlanterFund = artifacts.require("PlanterFund.sol");
+const Dai = artifacts.require("Dai.sol");
+
+module.exports = async function (deployer, network, accounts) {
+  const isLocal = network === "development";
+
+  const treeFactoryAddress = TreeFactory.address;
+  const planterFundsAddress = PlanterFund.address;
+  const treeAttributeAddress = TreeAttribute.address;
+  let daiTokenAddress;
+  let trustedForwarder;
+
+  if (isLocal) {
+    trustedForwarder = require("../build/gsn/Forwarder.json").address;
+    daiTokenAddress = Dai.address;
+  } else if (network == "mumbai") {
+    trustedForwarder = process.env.GSN_FORWARDER;
+    daiTokenAddress = Dai.address;
+  } else {
+    trustedForwarder = process.env.GSN_FORWARDER;
+    daiTokenAddress = eval(
+      `process.env.DAI_TOKEN_ADDRESS_${network.toUpperCase()}`
+    );
+  }
+
+  console.log("Call CommunityGifts Methods...");
+  await CommunityGifts.deployed().then(async (instance) => {
+    await instance.setTrustedForwarder(trustedForwarder);
+    await instance.setDaiTokenAddress(daiTokenAddress);
+    await instance.setTreeAttributesAddress(treeAttributeAddress);
+    await instance.setTreeFactoryAddress(treeFactoryAddress);
+    await instance.setPlanterFundAddress(planterFundsAddress);
+  });
+};
---
+++ migrations/28_run_fundPaymaster.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const WhitelistPaymaster = artifacts.require("WhitelistPaymaster.sol");
+
+module.exports = async function (deployer, network, accounts) {
+  const isLocal = network === "development";
+
+  console.log("Call Fund Paymaster Methods...");
+
+  let paymasterAddress = WhitelistPaymaster.address;
+
+  if (!isLocal) {
+    await web3.eth.sendTransaction(
+      {
+        from: accounts[0],
+        to: paymasterAddress,
+        value: web3.utils.toWei("1"),
+      },
+      (err, res) => {
+        console.log("err", err);
+        console.log("res", res);
+      }
+    );
+  }
+};

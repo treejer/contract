4330dfa91a9269174eb3124bb6a1c377ff0c4991
ali <abdolazim35@yahoo.com>
2021-09-22 16:55:51 +0330

    add new migrtions

---
+++ migrations/25_run_incrementalSellMethods.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+
+const IncrementalSell = artifacts.require("IncrementalSell.sol");
+const TreeFactory = artifacts.require("TreeFactory.sol");
+const WethFunds = artifacts.require("WethFunds.sol");
+const FinancialModel = artifacts.require("FinancialModel.sol");
+const Weth = artifacts.require("Weth.sol");
+const PlanterFund = artifacts.require("PlanterFund.sol");
+const TreeAuction = artifacts.require("TreeAuction.sol");
+const RegularSell = artifacts.require("RegularSell.sol");
+const TreeAttribute = artifacts.require("TreeAttribute.sol");
+
+module.exports = async function (deployer, network, accounts) {
+  const isLocal = network === "development";
+
+  let treeFactoryAddress = TreeFactory.address;
+  let financialModelAddress = FinancialModel.address;
+  let wethFundsAddress = WethFunds.address;
+  const planterFundAddress = PlanterFund.address;
+  const treeAuctionAddress = TreeAuction.address;
+  const regularSellAddress = RegularSell.address;
+  const treeAttributeAddress = TreeAttribute.address;
+  let wethTokenAddress;
+
+  //gsn
+  let trustedForwarder;
+
+  if (isLocal) {
+    trustedForwarder = require("../build/gsn/Forwarder.json").address;
+    wethTokenAddress = Weth.address;
+  } else if (network == "mumbai") {
+    trustedForwarder = process.env.GSN_FORWARDER;
+    wethTokenAddress = Weth.address;
+  } else {
+    trustedForwarder = process.env.GSN_FORWARDER;
+
+    wethTokenAddress = eval(
+      `process.env.WETH_TOKEN_ADDRESS_${network.toUpperCase()}`
+    );
+  }
+
+  console.log("Call IncrementalSell Methods...");
+  await IncrementalSell.deployed().then(async (instance) => {
+    await instance.setTrustedForwarder(trustedForwarder);
+    await instance.setPlanterFundAddress(planterFundAddress);
+    await instance.setRegularSellAddress(regularSellAddress);
+    await instance.setTreeFactoryAddress(treeFactoryAddress);
+    await instance.setWethFundsAddress(wethFundsAddress);
+    await instance.setWethTokenAddress(wethTokenAddress);
+    await instance.setFinancialModelAddress(financialModelAddress);
+    await instance.setTreeAttributesAddress(treeAttributeAddress);
+
+    await instance.addTreeSells(101, web3.utils.toWei("0.01"), 100, 10, 1000, {
+      from: accounts[0],
+    });
+  });
+};

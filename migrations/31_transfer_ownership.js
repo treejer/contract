a8cd3dfe64ae5ee08f45a8328136e4e7a13605c1
Amir Habibzadeh <habibzade.amir@gmail.com>
2022-01-14 21:02:10 +0300

    transfer ownership and change admin

---
+++ migrations/31_transfer_ownership.js
@@ -0,0 +1,14 @@
+// https://forum.openzeppelin.com/t/openzeppelin-truffle-upgrades/3579
+
+const { admin } = require('@openzeppelin/truffle-upgrades');
+
+module.exports = async function (deployer, network, accounts) {
+    // Use address of your Gnosis Safe
+    const gnosisSafe = process.env.SAFE_ADDRESS;
+
+    // Don't change ProxyAdmin ownership for our test network
+    if (network === 'matic') {
+        // The owner of the ProxyAdmin can upgrade our contracts
+        await admin.transferProxyAdminOwnership(gnosisSafe);
+    }
+};

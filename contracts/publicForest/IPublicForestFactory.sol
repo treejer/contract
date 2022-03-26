ec5c4c5d922dda1b396bd651c49080d1d269b9c9
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2022-03-26 13:04:49 +0430

    get referrer and some other data as input in fundTrees and complete comments of PublicForestFactory

--- contracts/publicForest/IPublicForestFactory.sol
+++ contracts/publicForest/IPublicForestFactory.sol
@@ -4,14 +4,63 @@ pragma solidity ^0.8.6;
 
 /** @title PublicForestFactory interface */
 interface IPublicForestFactory {
+    /**
+     * @dev emitted when implementation address updated
+     * @param implementation address of new implementation
+     */
     event ImplementationAddressUpdated(address implementation);
+
+    /**
+     * @dev emitted when baseToken address updated
+     * @param baseTokenAddress address of new baseToken
+     */
     event BaseTokenAddressUpdated(address baseTokenAddress);
+    /**
+     * @dev emitted when dexRouter address updated
+     * @param dexRouter address of new dexRouter
+     */
     event DexRouterAddressUpdated(address dexRouter);
+
+    /**
+     * @dev emitted when factory address of given forest updated
+     * @param forest address of forest to update it's factory
+     * @param factory address of new factory
+     */
+
     event FactoryAddressUpdated(address forest, address factory);
+
+    /**
+     * @dev emitted when validation of a token updated
+     * @param token address of token
+     * @param isValid if the token is valid or not
+     */
     event ValidTokensUpdated(address token, bool isValid);
+    /**
+     * @dev emitted when approve given to an erc721 token with id {tokenId}
+     * @param forest address of forest
+     * @param token address of token
+     * @param tokenId id of token
+     */
     event ERC721Approved(address forest, address token, uint256 tokenId);
+    /**
+     * @dev emitted when approve given to or revoking from an erc1155 token
+     * @param forest address of forest
+     * @param token address of token
+     */
     event ERC1155Approved(address forest, address token);
+
+    /**
+     * @dev emitted when new forest created
+     * @param forest address of created forest
+     * @param ipfsHash ipfsHash of forest
+     */
     event PublicForestCreated(address forest, string ipfsHash);
+
+    /**
+     * @dev emitted when ipfsHash of given forest updated
+     * @param forest address of forest to update it's ipfsHash
+     * @param ipfsHash new ipfsHash
+     */
     event IpfsHashUpdated(address forest, string ipfsHash);
 
     /**
@@ -29,20 +78,30 @@ interface IPublicForestFactory {
         address _treeTokenAddress
     ) external;
 
-    /** @dev set {_implementation} to implementation */
+    /**
+     * @dev set {_implementation} to implementation
+     * NOTE emit an {ImplementationAddressUpdated} event
+     */
     function setImplementationAddress(address _implementation) external;
 
-    /** @dev set {_baseTokenAddress} to baseTokenAddress */
+    /**
+     * @dev set {_baseTokenAddress} to baseTokenAddress
+     * NOTE emit a {BaseTokenAddressUpdated} event
+     */
     function setBaseTokenAddress(address _baseTokenAddress) external;
 
     /** @dev set {_regularSaleAddress} to regularSaleAddress */
     function setRegularSaleAddress(address _regularSaleAddress) external;
 
-    /** @dev set {_dexRouter} to dexRouter */
+    /**
+     * @dev set {_dexRouter} to dexRouter
+     * NOTE emit a {DexRouterAddressUpdated} event
+     */
     function setDexRouterAddress(address _dexRouter) external;
 
     /**
      * @dev update factory contract address for a given forest
+     * NOTE emit a {FactoryAddressUpdated} event
      * @param _forest address of PublicForest contract
      * @param _factory address of new factory contract
      */
@@ -50,6 +109,7 @@ interface IPublicForestFactory {
 
     /**
      * @dev update validation of a specific token
+     * NOTE emit a {ValidTokensUpdated} event
      * @param _token address of token
      * @param _isValid is valid token or not
      */
@@ -88,6 +148,7 @@ interface IPublicForestFactory {
     /**
      * @dev give approve to factory contract for an erc721 token with id of {_tokenId} in a
      * specific forest
+     * NOTE emit an {ERC721Approved} event
      * @param _forest address of PublicForest contract
      * @param _token address of token
      * @param _tokenId id of token
@@ -100,6 +161,7 @@ interface IPublicForestFactory {
 
     /**
      * @dev give approve to factory contract for an erc1155 token in a specific forest
+     * NOTE emit an {ERC1155Approved} event
      * @param _forest address of PublicForest contract
      * @param _token address of token
      */
@@ -108,14 +170,16 @@ interface IPublicForestFactory {
 
     /**
      * @dev create a forest with specific ipfsHash
+     * NOTE emit a {PublicForestCreated} event
      * @param _ipfsHash ipfsHash of forest
      */
     function createPublicForest(string memory _ipfsHash) external;
 
     /**
      * @dev update ipfsHash for a given forest
+     * NOTE emit an {IpfsHashUpdated} event
      * @param _forest address of PublicForest contract
-     * @param _ipfsHash new ipfs hash
+     * @param _ipfsHash new ipfsHash
      */
     function updateIpfsHash(address _forest, string memory _ipfsHash) external;
 

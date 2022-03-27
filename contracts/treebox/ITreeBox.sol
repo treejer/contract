aa78f9e9017d7a85687915cc310d550281ac8985
smuggler <mahdigorbanzadeh.1378@yahoo.com>
2022-03-27 14:12:11 +0430

    finish TreeBox contract

--- contracts/treebox/ITreeBox.sol
+++ contracts/treebox/ITreeBox.sol
@@ -3,11 +3,21 @@
 pragma solidity ^0.8.6;
 
 interface ITreeBox {
+    function initialize(address _token, address _admin) external;
+
     function claim(
         address _from,
         address _to,
         uint256 _tokenId
     ) external;
 
+    function updateCount(uint256 _amount) external;
+
+    function pause() external;
+
+    function unpause() external;
+
     function isTreeBox() external view returns (bool);
+
+    function isTreeBoxScipt(address _address) external view returns (bool);
 }
--- contracts/treebox/TreeBox.sol
+++ contracts/treebox/TreeBox.sol
@@ -2,47 +2,81 @@
 
 pragma solidity ^0.8.6;
 
-import "../access/IAccessRestriction.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+
 import "./ITreeBox.sol";
 import "./../tree/ITree.sol";
 
-contract TreeBox is ITreeBox {
+contract TreeBox is AccessControlUpgradeable, PausableUpgradeable, ITreeBox {
     bool public override isTreeBox;
-    IAccessRestriction public accessRestriction;
     ITree public treeToken;
+    bytes32 public constant TREEBOX_SCRIPT = keccak256("TREEBOX_SCRIPT");
+    mapping(address => uint256) private ownerToCount;
 
-    /** NOTE modifier to check msg.sender has data manager role */
-    modifier onlyDataManager() {
-        accessRestriction.ifDataManager(msg.sender);
+    /** NOTE modifier to check msg.sender has admin role */
+    modifier onlyAdmin() {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller not admin");
         _;
     }
 
-    /** NOTE modifier for check if function is not paused*/
     modifier ifNotPaused() {
-        accessRestriction.ifNotPaused();
+        require(!paused(), "Pausable: paused");
         _;
     }
 
-    constructor(address _accessRestrictionAddress, address _token) {
-        IAccessRestriction candidateContract = IAccessRestriction(
-            _accessRestrictionAddress
-        );
-        require(candidateContract.isAccessRestriction());
+    modifier onlyTreeBoxScript() {
+        require(isTreeBoxScipt(msg.sender), "Pausable: paused");
+        _;
+    }
+
+    function isTreeBoxScipt(address _address)
+        public
+        view
+        override
+        returns (bool)
+    {
+        return hasRole(TREEBOX_SCRIPT, _address);
+    }
+
+    function initialize(address _token, address _admin)
+        external
+        override
+        initializer
+    {
+        AccessControlUpgradeable.__AccessControl_init();
+        PausableUpgradeable.__Pausable_init();
 
         ITree candidateContractTree = ITree(_token);
+
         require(candidateContractTree.isTree());
 
-        accessRestriction = candidateContract;
+        if (!hasRole(DEFAULT_ADMIN_ROLE, _admin)) {
+            _setupRole(DEFAULT_ADMIN_ROLE, _admin);
+        }
+
         treeToken = candidateContractTree;
+    }
+
+    function pause() external override onlyAdmin {
+        _pause();
+    }
+
+    function unpause() external override onlyAdmin {
+        _unpause();
     }
 
     function claim(
         address _from,
         address _to,
         uint256 _tokenId
-    ) external override ifNotPaused onlyDataManager {
+    ) external override ifNotPaused onlyTreeBoxScript {
         require(_from != address(0) && _to != address(0), "Invalid address");
-
+        ownerToCount[msg.sender] -= 1;
         treeToken.safeTransferFrom(_from, _to, _tokenId);
+    }
+
+    function updateCount(uint256 _amount) external override {
+        ownerToCount[msg.sender] += _amount;
     }
 }

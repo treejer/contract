9d9bb2b5c5f4b662c03f42f38afec99472e790ef
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2022-03-27 14:37:27 +0430

    add test tree box

--- test/treeBox.js
+++ test/treeBox.js
@@ -1,8 +1,8 @@
 // const { accounts, contract, web3 } = require("@openzeppelin/test-environment");
 
 const AccessRestriction = artifacts.require("AccessRestriction");
+const TreeBox = artifacts.require("TreeBox");
 const Tree = artifacts.require("Tree");
-
 const assert = require("chai").assert;
 require("chai").use(require("chai-as-promised")).should();
 const truffleAssert = require("truffle-assertions");
@@ -15,8 +15,9 @@ const {
   erc721ErrorMsg,
 } = require("./enumes");
 
-contract("Tree", (accounts) => {
-  let treeBox;
+contract("TreeBox", (accounts) => {
+  let treeBoxInstance;
+  let treeInstance;
   let arInstance;
 
   const dataManager = accounts[0];
@@ -32,29 +33,47 @@ contract("Tree", (accounts) => {
 
   const zeroAddress = "0x0000000000000000000000000000000000000000";
 
-  before(async () => {});
-
   beforeEach(async () => {});
 
   afterEach(async () => {});
   //////////////////------------------------------------ deploy successfully ----------------------------------------//
 
-  it("deploys successfully", async () => {
-    const address = treeInstance.address;
-    assert.notEqual(address, 0x0);
-    assert.notEqual(address, "");
-    assert.notEqual(address, null);
-    assert.notEqual(address, undefined);
-  });
+  describe("deployment, set address, check access", () => {
+    before(async () => {
+      arInstance = await AccessRestriction.new({
+        from: deployerAccount,
+      });
 
-  it("deploy tree", async () => {
-    treeInstance = await Tree.new({
-      from: deployerAccount,
+      await arInstance.initialize(deployerAccount, {
+        from: deployerAccount,
+      });
+
+      treeInstance = await Tree.new({
+        from: deployerAccount,
+      });
+
+      await treeInstance.initialize(arInstance.address, "", {
+        from: deployerAccount,
+      });
+
+      treeBoxInstance = await TreeBox.new({
+        from: deployerAccount,
+      });
+
+      await treeBoxInstance.initialize(treeInstance.address, deployerAccount, {
+        from: deployerAccount,
+      });
     });
 
-    await treeInstance.initialize(zeroAddress, "", {
-      from: deployerAccount,
-    }).should.be.rejected;
-    ////////////////////////////////////////////////////////////////////////////////// ali
+    it("deploys successfully", async () => {
+      const address = treeBoxInstance.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
   });
+  ////////////////////////////////////////////////////////////////////////////////// ali
+
+  describe("claim", () => {});
 });

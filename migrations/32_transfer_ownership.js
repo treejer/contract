012b4b17660a233bea8177a9c826080ef0ddac4a
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2022-10-06 12:20:12 +0330

    fix migrations to deploy

--- migrations/32_transfer_ownership.js
+++ migrations/32_transfer_ownership.js
@@ -3,14 +3,12 @@ const { admin } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer, network, accounts) {
   // Use address of your Gnosis Safe
-
   // Don't change ProxyAdmin ownership for our test network
-  if (network != "development") {
-    const gnosisSafe = eval(
-      `process.env.SAFE_ADDRESS_${network.toUpperCase()}`
-    );
-
-    // The owner of the ProxyAdmin can upgrade our contracts
-    await admin.transferProxyAdminOwnership(gnosisSafe);
-  }
+  // if (network != "development") {
+  //   const gnosisSafe = eval(
+  //     `process.env.SAFE_ADDRESS_${network.toUpperCase()}`
+  //   );
+  //   // The owner of the ProxyAdmin can upgrade our contracts
+  //   await admin.transferProxyAdminOwnership(gnosisSafe);
+  // }
 };
